abcdf0a1fa4408b9873df3d48c60bb5251ce9560
Pawel Wieczorek <236691@edu.p.lodz.pl>
2022-03-07 19:36:46 +0100

    initial commit

---
+++ View/src/main/java/com/mycompany/sudoku/view/HelloController.java
@@ -0,0 +1,40 @@
+package com.mycompany.sudoku.view;
+
+
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
+
+
+    @FXML
+    private Button button1;
+
+    @FXML
+    private Button button2;
+
+    @FXML
+    private Button button3;
+
+
+
+    /**
+     * Initialization.
+     *
+     * @param url            url data.
+     * @param resourceBundle resources language.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+
+
+
+
+}
