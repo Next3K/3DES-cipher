7ddaef07d52b1d52ed83950c0f071a0dad1f91da
Pawel Wieczorek <236691@edu.p.lodz.pl>
2022-03-14 10:45:24 +0100

    added function that generates 56 bit key from main 64 bit key

--- Model/src/main/java/kryptografia/model/cipher/DES.java
+++ Model/src/main/java/kryptografia/model/cipher/DES.java
@@ -1,12 +1,34 @@
 package kryptografia.model.cipher;
 
+import kryptografia.model.helper.Helper;
+import kryptografia.model.helper.Tables;
+
 public class DES {
 
-    private final String key;
+    private final long mainKey;
+    private String message;
+    private long[] subKeys = new long[16];
+    private long smallerMainKey; // 56 bit key created form main key.
 
-    public DES(String key) {
-        this.key = key;
+    public DES(long key, String message) {
+        this.message = message;
+        this.mainKey = key;
     }
 
+    /**
+     * Generates 56 bit sub-key from main key.
+     */
+    public void generateSmallerMainKey() {
+        long smallerMainKey = 0;
+        for (int i = 0; i < Tables.PC1.length; i++) {
+            byte valueToSet = (byte) Helper.getBit(mainKey,64 - Tables.PC1[i]);
+            smallerMainKey = Helper.setBit(smallerMainKey,55 - i,valueToSet);
+        }
+        this.smallerMainKey = smallerMainKey;
+    }
+    
 
+    public long getSmallerMainKey() {
+        return smallerMainKey;
+    }
 }
