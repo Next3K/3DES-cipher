25258f1a87427f5df5e51278f7fd88106d3244cc
levitatemoon <100847106+levitatemoon@users.noreply.github.com>
2022-03-20 17:56:20 +0100

    Update HelloController.java

--- View/src/main/java/com/mycompany/sudoku/view/HelloController.java
+++ View/src/main/java/com/mycompany/sudoku/view/HelloController.java
@@ -7,33 +7,67 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
 
 public class HelloController implements Initializable {
 
-
-    @FXML
-    private Button button1;
-
-    @FXML
-    private Button button2;
-
-    @FXML
-    private Button button3;
-
-
-
-    /**
-     * Initialization.
-     *
-     * @param url            url data.
-     * @param resourceBundle resources language.
-     */
+    @FXML private Button wczytajKluczeButton;
+    @FXML private Button zapiszKluczeButton;
+    @FXML private Button szyfrujButton;
+    @FXML private Button deszyfrujButton;
+    @FXML private Button wczytajJawneButton;
+    @FXML private Button zapiszJawneButton;
+    @FXML private Button wczytajSzyfrogramButton;
+    @FXML private Button zapiszSzyfrogramButton;
+    @FXML private TextField klucz1;
+    @FXML private TextField klucz2;
+    @FXML private TextField klucz3;
+    @FXML private TextField kluczPlik;
+    @FXML private TextField tekstJawny;
+    @FXML private TextField szyfrogram;
+    @FXML private TextField plikJawny;
+    @FXML private TextField plikSzyfrogram;
+    @FXML private RadioButton okienkoRadioButton;
+    @FXML private RadioButton plikRadioButton;
+    
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
 
+    public void szyfruj(ActionEvent event) {
+        
+    }
+    
+    public void deszyfruj(ActionEvent event) {
+        
+    }
 
+    public void wczytajKlucze(ActionEvent event) {
+        
+    }
+    
+    public void zapiszKlucze(ActionEvent event) {
+        
+    }
+    
+    public void zapiszJawne(ActionEvent event) {
+        
+    }
+    
+    public void wczytajJawne(ActionEvent event) {
+        
+    }
+    
+    public void zapiszSzyfr(ActionEvent event) {
+        
+    }
+    
+    public void wczytajSzyfr(ActionEvent event) {
+        
+    }
 
 
 
