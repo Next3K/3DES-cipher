d20ba219b9a1aa70f1971406dc7b1491551c4f79
Pawel Wieczorek <236691@edu.p.lodz.pl>
2022-04-05 19:19:01 +0200

    example of encrytpion with key generation from text

--- Model/src/main/java/kryptografia/model/Main.java
+++ Model/src/main/java/kryptografia/model/Main.java
@@ -1,20 +1,36 @@
 package kryptografia.model;
 
 import kryptografia.model.cipher.ThreeDes;
-
+import kryptografia.model.helper.Helper;
 
 public class Main {
     public static void main(String[] args) {
 
-        long key1 = 0xAABB09182736CCDDL;
-        long key2 = 0x9182736A0CCDABBDL;
-        long key3 = 0x6CCA9182A73BB0DDL;
+        long[] keys = Helper.generateKeysFromPassword("passwordToCipher");
+
         long data64bit = 0x123456ABCD132536L;
+        long key1 = keys[0];
+        long key2 = keys[1];
+        long key3 = keys[2];
+
+        System.out.print("Key one  : ");
+        Helper.printBits(key1);
+
+        System.out.print("Key two  : ");
+        Helper.printBits(key2);
+
+        System.out.print("Key three: ");
+        Helper.printBits(key3);
+
 
         ThreeDes threeDes = new ThreeDes(key1, key2, key3);
-        long decryptedData = threeDes.decrypt(threeDes.encrypt(data64bit));
+
+        long encryptedData = threeDes.encrypt(data64bit);
+        long decryptedData = threeDes.decrypt(encryptedData);
+
 
         System.out.println("3DES Symmetrical encryption-decryption status: " +
                 ((data64bit == decryptedData) ? "success" : "failure"));
     }
+
 }
